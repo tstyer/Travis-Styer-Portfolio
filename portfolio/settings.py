fadc7915289187dbf66bfcb795d7e87a66af1de0
tstyer <travisstyer.ts@gmail.com>
2025-11-08 08:43:49 +0000

    Adjust settings, urls, views

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=q5h-#$kl-i*%jvvw2=msuv0cw%ww41q&rnpvg^_&5_m4!2e8%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
